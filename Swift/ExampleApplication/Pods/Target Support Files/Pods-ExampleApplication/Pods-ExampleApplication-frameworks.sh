cd428fdc5bb06e46a7a133ac4ea08526797c6e3f
levent <lewentoral@gmail.com>
2018-12-19 17:40:48 +0300

    remove pods

--- Swift/ExampleApplication/Pods/Target Support Files/Pods-ExampleApplication/Pods-ExampleApplication-frameworks.sh
+++ Swift/ExampleApplication/Pods/Target Support Files/Pods-ExampleApplication/Pods-ExampleApplication-frameworks.sh
@@ -143,10 +143,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/AppSamuraiAdSDK/AppSamuraiAdSDK.framework"
+  install_framework "${PODS_ROOT}/AppSamuraiAdSDK/Frameworks/AppSamuraiAdSDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/AppSamuraiAdSDK/AppSamuraiAdSDK.framework"
+  install_framework "${PODS_ROOT}/AppSamuraiAdSDK/Frameworks/AppSamuraiAdSDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
