fb3d6d6aae90a22ac1cd394448a774777aac0a4b
Burak Dirin <burakdirin@gmail.com>
2021-09-23 17:21:59 +0300

    storyly swift package dependency update

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "StorylyTargets",
             dependencies: [
-                "Storyly",
+                .target(name: "Storyly"),
                 .product(name: "SDWebImage", package: "SDWebImage")
             ],
             path: "Sources"
