5e887dc24df68c6486251148d554b9b4d0d47d5f
Burak Dirin <burakdirin@gmail.com>
2021-09-23 15:58:35 +0300

    storyly swift package dependency update

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
                 .target(name: "Storyly"),
                 .product(name: "SDWebImage", package: "SDWebImage")
             ],
-            path: "Storyly/Storyly"
+            path: "Storyly/Source"
         )
     ]
 )
